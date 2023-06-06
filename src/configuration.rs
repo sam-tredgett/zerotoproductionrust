6904ccf37edc0aea3c979a88fa78a35ab7221581
dabi-ngin <satre@pop-os.localdomain>
2023-06-06 22:56:29 +0100

    rustfmt run + github actions modification

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,8 +1,7 @@
-
 #[derive(serde::Deserialize)]
 pub struct Settings {
     pub database: DatabaseSettings,
-    pub application_port: u16
+    pub application_port: u16,
 }
 
 #[derive(serde::Deserialize)]
@@ -16,9 +15,10 @@ pub struct DatabaseSettings {
 
 pub fn get_configuration() -> Result<Settings, config::ConfigError> {
     let settings = config::Config::builder()
-        .add_source(
-            config::File::new("configuration.yaml", config::FileFormat::Yaml)
-        )
+        .add_source(config::File::new(
+            "configuration.yaml",
+            config::FileFormat::Yaml,
+        ))
         .build()?;
 
     settings.try_deserialize::<Settings>()
@@ -27,15 +27,15 @@ pub fn get_configuration() -> Result<Settings, config::ConfigError> {
 impl DatabaseSettings {
     pub fn connection_string(&self) -> String {
         format!(
-        "postgres://{}:{}@{}:{}/{}",
-        self.username, self.password, self.host, self.port, self.database_name
-            )
+            "postgres://{}:{}@{}:{}/{}",
+            self.username, self.password, self.host, self.port, self.database_name
+        )
     }
 
     pub fn connection_string_without_db(&self) -> String {
         format!(
             "postgres://{}:{}@{}:{}",
             self.username, self.password, self.host, self.port
-       )
+        )
     }
 }
--- src/startup.rs
+++ src/startup.rs
@@ -1,10 +1,10 @@
-use actix_web::{web, App,  HttpServer };
-use sqlx::PgPool;
+use crate::endpoints::{health_check, subscribe};
 use actix_web::dev::Server;
 use actix_web::middleware::Logger;
+use actix_web::{web, App, HttpServer};
+use sqlx::PgPool;
 use std::net::TcpListener;
-use crate::endpoints::{health_check, subscribe};
- 
+
 pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Error> {
     let db_pool = web::Data::new(db_pool);
     let server = HttpServer::new(move || {
@@ -14,7 +14,7 @@ pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Er
             .route("/subscriptions", web::post().to(subscribe))
             .app_data(db_pool.clone())
     })
-        .listen(listener)?
-        .run();
+    .listen(listener)?
+    .run();
     Ok(server)
 }
