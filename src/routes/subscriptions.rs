7b09fc475fa1e0a6be7d79c06134cae99166924b
SamTredgett <sam.tredgett@gmail.com>
2024-06-01 21:18:50 +0100

    testing something

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -51,13 +51,12 @@ pub async fn insert_subscriber(
 ) -> Result<(), sqlx::Error> {
     sqlx::query!(
         r#"
-        INSERT INTO subscriptions (id, email, name, subscribed_at, status)
-        VALUES ($1, $2, $3, $4, $5)"#,
+        INSERT INTO subscriptions (id, email, name, subscribed_at)
+        VALUES ($1, $2, $3, $4)"#,
         Uuid::new_v4(),
         new_subscriber.email.as_ref(),
         new_subscriber.name.as_ref(),
-        Utc::now(),
-        "pending_confirmation"
+        Utc::now()
     )
     .execute(pool)
     .await
