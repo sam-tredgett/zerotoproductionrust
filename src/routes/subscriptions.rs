336c9d9605113f393f40739da0bcb24acd51fc32
SamTredgett <sam.tredgett@gmail.com>
2024-06-01 21:00:07 +0100

    updated to pass in string rather than inline vvalue

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -52,11 +52,12 @@ pub async fn insert_subscriber(
     sqlx::query!(
         r#"
         INSERT INTO subscriptions (id, email, name, subscribed_at, status)
-        VALUES ($1, $2, $3, $4, 'pending_confirmation')"#,
+        VALUES ($1, $2, $3, $4, $5)"#,
         Uuid::new_v4(),
         new_subscriber.email.as_ref(),
         new_subscriber.name.as_ref(),
-        Utc::now()
+        Utc::now(),
+        "pending_confirmation"
     )
     .execute(pool)
     .await
